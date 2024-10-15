ae7eeae44db7c9bf3a9845884d0105609b8e0c2a
DESKTOP-ANIMP3N\fabio <fjrocha9@gmail.com>
2024-10-15 23:25:35 +0100

    Added validation and swagger.

--- src/main/kotlin/com/faroc/flyme/airline/AirlineController.kt
+++ src/main/kotlin/com/faroc/flyme/airline/AirlineController.kt
@@ -4,7 +4,16 @@ import com.faroc.flyme.airline.requests.AddAirlineRequest
 import com.faroc.flyme.airline.responses.AirlinesResponse
 import com.faroc.flyme.common.errors.problem
 import com.github.michaelbull.result.fold
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
+import org.springframework.http.ProblemDetail
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -14,13 +23,46 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 
-@RestController
+@RestController("Airlines Controller V1")
 @RequestMapping("v1/airlines")
+@Tag(name = "Airlines")
 class AirlineController(private val service: AirlineService) {
-
+    @Operation(summary = "Add airlines to platform.", description = "Add airlines to platform")
+    @ApiResponses(value = [
+        ApiResponse(
+                responseCode = "201",
+                description = "Added airlines.",
+                content = [(Content(
+                    mediaType = "application/json",
+                    array = ArraySchema(schema = Schema(implementation = AirlinesResponse::class))))
+                ]),
+        ApiResponse(
+            responseCode = "400",
+            description = "Added airlines.",
+            content = [(Content(
+                mediaType = "application/json",
+                schema = Schema(implementation = ProblemDetail::class)))
+            ]),
+        ApiResponse(
+                responseCode = "500",
+                description = "Failed to add airlines due to internal error.",
+                content = [(Content(
+                    mediaType = "application/json",
+                    schema = Schema(implementation = ProblemDetail::class)))
+                ]),
+        ]
+    )
     @PostMapping
-    suspend fun addAirlines(@RequestBody request: List<AddAirlineRequest>) : ResponseEntity<List<AirlinesResponse>> {
+    suspend fun addAirlines(
+        @Valid @RequestBody request: List<AddAirlineRequest>) : ResponseEntity<List<AirlinesResponse>> {
         return ResponseEntity(service.addAirlines(request), HttpStatus.CREATED)
+    }
+
+    @PostMapping("test")
+    suspend fun addAirlines2() : MutableMap<String, String> {
+        val a = mutableMapOf("a" to "b")
+
+        return a
     }
 
     @GetMapping
--- src/main/kotlin/com/faroc/flyme/airline/requests/AddAirlineRequest.kt
+++ src/main/kotlin/com/faroc/flyme/airline/requests/AddAirlineRequest.kt
@@ -1,6 +1,13 @@
 package com.faroc.flyme.airline.requests
 
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
 data class AddAirlineRequest(
+    @field:NotBlank(message = "Name must not be empty or omitted.")
+    @field:Size(message = "Name must not be between 1 and 50 characters.", min = 1, max = 50)
     val name: String,
+    @field:NotBlank(message = "Country must not be empty or omitted.")
+    @field:Size(message = "Country must not be between 1 and 50 characters.", min = 1, max = 50)
     val country: String,
 )
